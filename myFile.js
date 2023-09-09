cc12241798fd876088488a41bc5d8c5d9802f86a
Slimobedsama <ohwo.okoro@yahoo.com>
2023-09-09 23:51:32 +0100

    installation of npm moment package and implementation

--- myFile.js
+++ myFile.js
@@ -1,13 +1,13 @@
-const day = new Date();
-const today = day.getDay();
+const moment = require('moment');
+const today = moment().day();
 const daysOfWeek = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
-const profile = [{
-    slack_name: 'Obedsama',
+const profile = {
+    slack_name: "Obedsama",
     current_day: daysOfWeek[today],
-    utc_time: new Date().toISOString(),
-    track: 'backend',
-    github_file_url: 'https://github.com/Slimobedsama/hng_backend_task_1/blob/main/server.js',
-    github_repo_url: 'https://github.com/Slimobedsama/hng_backend_task_1',
+    utc_time: moment().utc(),
+    track: "backend",
+    github_file_url: "https://github.com/Slimobedsama/hng_backend_task_1/blob/main/server.js",
+    github_repo_url: "https://github.com/Slimobedsama/hng_backend_task_1",
     status_code: 200
-}]
+}
 module.exports = profile;
