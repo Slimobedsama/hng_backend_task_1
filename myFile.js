883fd39c40f93fb380d9c28b80d92578f6e18fd8
Slimobedsama <ohwo.okoro@yahoo.com>
2023-09-10 01:36:39 +0100

    change the utc_time to new Date method

--- myFile.js
+++ myFile.js
@@ -1,10 +1,11 @@
 const moment = require('moment');
+const time = new Date();
 const today = moment().day();
 const daysOfWeek = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
 const profile = {
     slack_name: "Obedsama",
     current_day: daysOfWeek[today],
-    utc_time: moment().utc().format(),
+    utc_time: time.toISOString(),
     track: "backend",
     github_file_url: "https://github.com/Slimobedsama/hng_backend_task_1/blob/main/server.js",
     github_repo_url: "https://github.com/Slimobedsama/hng_backend_task_1",
