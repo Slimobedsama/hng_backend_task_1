a9ee0c9a8333822d122ec6116f761f076f2f21d3
Slimobedsama <ohwo.okoro@yahoo.com>
2023-09-10 13:44:42 +0100

    correction of utc format

--- myFile.js
+++ myFile.js
@@ -1,11 +1,11 @@
 const moment = require('moment');
-const time = new Date();
+const time = moment().utc().format();
 const today = moment().day();
 const daysOfWeek = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
 const profile = {
     slack_name: "Obedsama",
     current_day: daysOfWeek[today],
-    utc_time: time.toISOString().slice(0, 19) + 'Z',
+    utc_time: time,
     track: "backend",
     github_file_url: "https://github.com/Slimobedsama/hng_backend_task_1/blob/main/server.js",
     github_repo_url: "https://github.com/Slimobedsama/hng_backend_task_1",
