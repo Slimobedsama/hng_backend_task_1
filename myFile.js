daa831d1520eabdec7aedf466e99efe37c394c66
Slimobedsama <ohwo.okoro@yahoo.com>
2023-09-10 12:59:02 +0100

    correction of utc format using splice method

--- myFile.js
+++ myFile.js
@@ -5,7 +5,7 @@ const daysOfWeek = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Fri
 const profile = {
     slack_name: "Obedsama",
     current_day: daysOfWeek[today],
-    utc_time: moment().utc().format(),
+    utc_time: time.toISOString().slice(0, 19) + 'Z',
     track: "backend",
     github_file_url: "https://github.com/Slimobedsama/hng_backend_task_1/blob/main/server.js",
     github_repo_url: "https://github.com/Slimobedsama/hng_backend_task_1",
